476da441c9947df8eac75b21f2c14d2010642414
erick-marin <emarin.iot@gmail.com>
2021-10-07 17:25:30 -0400

    Renamed file to pdm_init.sh

---
+++ scripts/pdm_init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Poetry provides a custom installer that will install poetry isolated from the rest of your system.
+cd /home/vagrant
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python3 -

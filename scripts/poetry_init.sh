14fb42d3b8f530f79c24505f92dee2dbbb78b2cf
erick-marin <emarin.iot@gmail.com>
2021-10-07 08:47:05 -0400

    Changed piped command to 'python3'

--- scripts/poetry_init.sh
+++ scripts/poetry_init.sh
@@ -2,4 +2,4 @@
 
 # Poetry provides a custom installer that will install poetry isolated from the rest of your system.
 cd /home/vagrant
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python3 -
